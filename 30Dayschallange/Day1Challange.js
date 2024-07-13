7f8df445676315bc57b0c8c3eea2365294d56555
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-13 11:18:53 +0000

    day 1 chalange code

---
+++ 30Dayschallange/Day1Challange.js
@@ -0,0 +1,48 @@
+/*Activity 1  Variable Decleration */
+
+// Tast 1 - Declare a var using var assign it a number and log value to thr console
+//Task 2 -  Declare a var using let assign it a string and log the value to the console 
+
+
+// var number = 25
+
+// let MyName = "Satish"
+
+// console.log(number);
+// console.log(MyName);
+
+// //Task 3 - declare a variable using const and assign a boolean value and console log it 
+
+// const LoggedIn = true;
+
+// console.log( typeof LoggedIn );
+
+// //Task 4 create a variable for different datatype and number string boolean array object and typeof and log
+
+// const Myname = "Satish"
+
+// const LogOut = false 
+
+// let MyPassward = 12345678
+
+// const cardnumber = [ 22,45,10,33,99];
+
+// let StudentId = {
+//     studentname : "satish",
+//     rollno : 21,
+//     fathername : "chai aur Code",
+//     address : "kamala nehru nagar Delhi",
+//     phoneno : 636248626782
+
+// }
+
+
+// console.log( typeof Myname);
+// console.log( typeof LogOut);
+// console.log(typeof MyPassward);
+// console.log(typeof cardnumber);
+// console.log( typeof StudentId);
+
+
+//task 5 declare a variable using let assign and initlize reasing nes value and log both the value
+
