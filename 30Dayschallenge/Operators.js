5eb2d8e339fc8b01146b4ac0bf8269c8dae002a2
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-14 09:49:35 +0000

    operators

---
+++ 30Dayschallenge/Operators.js
@@ -0,0 +1,31 @@
+// task 1 to task 5 add subtract divide muktiply and reminder
+
+console.log(2 +2);
+console.log(10-5);
+console.log(20/5);
+console.log(100*20);
+console.log(10%3);
+
+
+// task 6 and 7
+let positive = 20
+let add = 1
+let number = positive += add;
+console.log(number);
+
+
+let negative = 20
+let sub = 1
+let value = negative -= sub;
+console.log(value);
+
+
+// task - 8, 9 , 10
+console.log(10>5);
+console.log(10<5);
+console.log(10<=10);
+console.log(10>=5);
+console.log("10"==10);
+console.log("10"===10);
+
+//task 
