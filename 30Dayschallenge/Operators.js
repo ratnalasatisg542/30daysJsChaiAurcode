7da2d1730cf8f4c8b2cc8e5de0a583206dbc518c
Ratnala Satish <ratnalasatish542@gmail.com>
2024-07-14 14:22:27 +0000

    day 2 completed

--- 30Dayschallenge/Operators.js
+++ 30Dayschallenge/Operators.js
@@ -1,31 +1,85 @@
 // task 1 to task 5 add subtract divide muktiply and reminder
 
-console.log(2 +2);
-console.log(10-5);
-console.log(20/5);
-console.log(100*20);
-console.log(10%3);
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log("sum:" + sum);
+
+let num3 = 10;
+let num4 = 10;
+let sub = num3 - num4;
+console.log("sub:" + sub);
 
 
-// task 6 and 7
+let num5 = 10;
+let num6 = 20;
+let div = num1 / num2;
+console.log("divide:" + div);
+
+
+let num7 = 10;
+let num8 = 20;
+let multi = num7 * num8;
+console.log("multiply:" + multi);
+
+
+let num9 = 100;
+let num10 = 3;
+let reminder = num9 % num10;
+console.log("reminder:" + reminder);
+
+
+
+// // task 6 and 7
 let positive = 20
-let add = 1
-let number = positive += add;
+let add
+let number = positive +=10;
 console.log(number);
 
 
-let negative = 20
-let sub = 1
-let value = negative -= sub;
-console.log(value);
+let x = 10;
+console.log("x = " + x);
+x -= 5; 
+console.log("x += 5: " + x); 
+
 
 
 // task - 8, 9 , 10
-console.log(10>5);
-console.log(10<5);
-console.log(10<=10);
-console.log(10>=5);
-console.log("10"==10);
-console.log("10"===10);
+// console.log(10>5);
+// console.log(10<5);
+// console.log(10<=10);
+// console.log(10>=5);
+// console.log("10"==10);
+// console.log("10"===10);
 
-//task 
+// let e = 5;
+// let f = '5';
+// console.log("e === f: " + (e === f)); 
+// console.log("e !== f: " + (e !== f));
+
+// //task 11 12 13
+
+let age = 25;
+let hasLicense = true;
+let canRent = age >= 21 && hasLicense;
+console.log("Can rent a car: " + canRent); 
+
+
+let isStudent = false;
+let isTeacher = true;
+let hasAccess = isStudent || isTeacher;
+console.log("Has access: " + hasAccess); 
+
+
+let isRaining = true;
+let goOutside = !isRaining;
+console.log("Go outside: " + goOutside); 
+
+
+
+
+
+// //task 14
+// let marks = -5;
+// let result = marks >0 ? "pass ": "fail";
+// console.log(result);
